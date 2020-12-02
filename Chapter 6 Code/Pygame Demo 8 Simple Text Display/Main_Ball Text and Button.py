6f20029dbccada36aa5a1ca17739e8be3e4fc203
Irv Kalb <Irv@furrypants.com>
2020-12-02 11:36:13 -0800

    Cleaned up ball example
    Cleaned up ball example to be simpler

--- Chapter 6 Code/Pygame Demo 8 Simple Text Display/Main_Ball Text and Button.py
+++ Chapter 6 Code/Pygame Demo 8 Simple Text Display/Main_Ball Text and Button.py
@@ -1,4 +1,4 @@
-# pygame demo 8 Ball Text and Button
+# pygame demo 8 Drawing Simple text
 
 # 1 - Import packages
 import pygame
@@ -6,16 +6,16 @@ from pygame.locals import *
 import sys
 import random
 from Ball import *  # bring in the Ball class code
-from SimpleText import * # bring the SimpleText class code
-from SimpleButton import * # bring the SimpleButton class code
+from SimpleText import *
+
 
 # 2 - Define constants
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
-FRAMES_PER_SECOND = 30
-
+FRAMES_PER_SECOND = 30 
+       
 # 3 - Initialize the world
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -24,11 +24,11 @@ clock = pygame.time.Clock()  # set the speed (frames per second)
 # 4 - Load assets: image(s), sounds, etc.
 
 # 5 - Initialize variables
-oBall = Ball(window, WINDOW_WIDTH, WINDOW_HEIGHT)
-frameCountDisplay = SimpleText(window, (100, 15), '', WHITE)
-restartButton = SimpleButton(window, (290, 444), \
-                             'images/restartUp.png', 'images/restartDown.png')
 frameCounter = 0
+oBall = Ball(window, WINDOW_WIDTH, WINDOW_HEIGHT)
+frameCountLabel = SimpleText(window, (100, 15),
+                            'Program has gone through this many loops:', WHITE)
+frameCountDisplay = SimpleText(window, (300, 35), '0', WHITE)
 
 # 6 - Loop forever
 while True:
@@ -37,29 +37,25 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
-
-        if restartButton.handleEvent(event):
-            frameCounter = 0  # clicked button, reset counter
+            sys.exit()          
 
     # 8 - Do any "per frame" actions
     oBall.update()  # tell the ball to update itself
-    messageText = 'Program has run through this many loops: ' + str(frameCounter)
-    frameCountDisplay.setValue(messageText)
+    frameCountDisplay.setValue(str(frameCounter))
     frameCounter = frameCounter + 1  # increment each frame
 
    # 9 - Clear the screen before drawing it again
     window.fill(BLACK)
-    frameCountDisplay.draw()
     
     # 10 - Draw the screen elements
     oBall.draw()   # tell the ball to draw itself
-    restartButton.draw()
+    frameCountLabel.draw()
+    frameCountDisplay.draw()
 
     # 11 - Update the screen
     pygame.display.update()
 
     # 12 - Slow things down a bit
-    clock.tick(FRAMES_PER_SECOND)  # make pygame wait the correct amount
+    clock.tick(FRAMES_PER_SECOND)  # make PyGame wait the correct amount
 
 
